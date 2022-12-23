681903364f98ebd9ee6ab58c26e194f8af7c583b
Lacatis, Danuta Tina <danutatina.lacatis@iso-gruppe.com>
2022-12-23 10:19:36 +0100

    make add task button working

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker"></Header>
+    <Header @toggle-add-task="toggleAddTask v:showAddTask="showAddTask"></Header>
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+
     <Tasks
       @toggle-reminder="toggleReminder"
       @delete-task="deleteTask"
@@ -12,19 +16,28 @@
 <script>
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
   name: "App",
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false,
     };
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
     deleteTask(id) {
       if (confirm("Are you sure")) {
         this.tasks = this.tasks.filter((task) => task.id !== id);
